880214dcf368f43af0dff9155e9a8e795cf4a354
Sher Muhammad <sherabro141@gmail.com>
2023-07-19 21:27:03 +0500

    Todo App

---
+++ app.js
@@ -0,0 +1,54 @@
+
+var klist = document.getElementById("neww")
+function addition(){
+    var add = document.getElementById("input")
+    if(add.value === ""){
+        alert("Please type something")
+    }
+
+    var check = document.createElement("input")
+    check.setAttribute("type" , "checkbox")
+
+
+        var btn = document.createElement("input")
+        btn.setAttribute("type" , "button")
+        btn.setAttribute("value" , "del")
+        btn.setAttribute("onclick" , "dele(this)")
+        btn.setAttribute("class" , "delbtn")
+
+        var editbtn = document.createElement("input")
+        editbtn.setAttribute("type" , "button")
+        editbtn.setAttribute("value" , "edit")
+        editbtn.setAttribute("onclick" , "edit(this)")
+        editbtn.setAttribute("class" , "btns")
+        
+        // editbtn.style.marginLeft = "60%"
+        var newpara = document.createElement("p")
+        var para = document.createTextNode(add.value)
+        newpara.appendChild(check)
+        newpara.appendChild(para)
+        newpara.appendChild(btn)
+        newpara.appendChild(editbtn)
+        // newpara.appendChild(edit())
+        var sher = document.getElementById("neww");
+        var sher1 = sher.appendChild(newpara)
+        sher.appendChild(newpara)
+        add.value = "";
+    }
+    function dele(e){
+        e.parentNode.remove()
+        console.log(e)
+    }
+    function edit(e){
+    //    var val =  e.parentNode.childNodes[1]
+    //    var promp = prompt("",val.nodeValue)
+    //     val.nodeValue = promp
+    var val = klist.childNodes
+    var promp = prompt("",val.nodeValue)
+    val = promp
+    console.log(val)
+    }
+    function DeleteAll(){
+      klist.innerHTML = "";
+        
+    }
